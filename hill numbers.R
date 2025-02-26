f3711eb90d83a1266cd6bfbd9773a1018589454a
DanielaCortesGuzman <33530787+DanielaCortesGuzman@users.noreply.github.com>
2025-02-26 08:55:48 +0100

    Add files via upload

---
+++ hill numbers.R
@@ -0,0 +1,13 @@
+
+library(hillR)
+library(ape)
+library(phytools)
+
+phy.tree<-read.nexus(file.choose(), tree.names = NULL, force.multi = FALSE) # output.nex
+consensus.tree<-consensus(phy.tree, p = 0.5, check.labels = TRUE, rooted = TRUE)
+t1<-consensus.edges(phy.tree)
+
+hill_phylo(comm, t1, q = 0)
+hill_phylo_parti(comm, t1, q = 0)
+
+write.csv(comm, "comm.csv", row.names = FALSE)
